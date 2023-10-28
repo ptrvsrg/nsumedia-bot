e19a93423854a3541b0bb68e52fe2d69842d76c1
ptrvsrg <s.petrov1@g.nsu.ru>
2023-10-28 14:48:21 +0700

    Удалил неиспользуемые импорты

--- src/main/java/ru/nsu/ccfit/nsumediabot/NsumediaBotApplication.java
+++ src/main/java/ru/nsu/ccfit/nsumediabot/NsumediaBotApplication.java
@@ -2,10 +2,6 @@ package ru.nsu.ccfit.nsumediabot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.io.File;
 
 @SpringBootApplication
 public class NsumediaBotApplication {
