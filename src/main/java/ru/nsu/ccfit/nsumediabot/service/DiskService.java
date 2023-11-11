25dd8563ef6b4ab141da1b4ae26a02694ab1d191
ptrvsrg <s.petrov1@g.nsu.ru>
2023-11-11 21:18:18 +0700

    Изменил сигнатуры методов сервиса

--- src/main/java/ru/nsu/ccfit/nsumediabot/service/DiskService.java
+++ src/main/java/ru/nsu/ccfit/nsumediabot/service/DiskService.java
@@ -1,11 +1,10 @@
 package ru.nsu.ccfit.nsumediabot.service;
 
-import ru.nsu.ccfit.nsumediabot.models.dto.DiskFileDTO;
-
 import java.io.File;
 
 public interface DiskService {
-    DiskFileDTO upload(File file);
 
-    void delete(String diskFilePath);
+    String uploadFile(String path, File file);
+
+    void deleteFile(String path);
 }
