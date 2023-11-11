9c3b01fae9b3e8f2a8f733e02b45ef376fc21c06
ptrvsrg <s.petrov1@g.nsu.ru>
2023-11-11 21:18:51 +0700

    Добавил POJO класс для ответа Yandex Disk API при создании директорий

---
+++ src/main/java/ru/nsu/ccfit/nsumediabot/models/responses/CreateDirectoryResponse.java
@@ -0,0 +1,18 @@
+package ru.nsu.ccfit.nsumediabot.models.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateDirectoryResponse {
+
+    private String href;
+    private String method;
+    private boolean templated;
+
+}
