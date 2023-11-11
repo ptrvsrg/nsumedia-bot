46c8f1c605638c39c7f0fdaa6f0f589a2089ae49
ptrvsrg <s.petrov1@g.nsu.ru>
2023-11-11 21:21:28 +0700

    Реализовал создание директорий; Вынес маппинг JSON ответов в POJO; Рефакторинг

--- src/main/java/ru/nsu/ccfit/nsumediabot/service/impl/DiskServiceImpl.java
+++ src/main/java/ru/nsu/ccfit/nsumediabot/service/impl/DiskServiceImpl.java
@@ -1,167 +1,209 @@
 package ru.nsu.ccfit.nsumediabot.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
+import com.squareup.okhttp.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.nsu.ccfit.nsumediabot.models.dto.DiskFileDTO;
 import ru.nsu.ccfit.nsumediabot.models.exceptions.DiskException;
 import ru.nsu.ccfit.nsumediabot.models.responses.FileInfoResponse;
 import ru.nsu.ccfit.nsumediabot.models.responses.GetUploadUrlResponse;
-import ru.nsu.ccfit.nsumediabot.models.responses.PublishFileResponse;
-import ru.nsu.ccfit.nsumediabot.models.responses.YandexDiskErrorResponse;
+import ru.nsu.ccfit.nsumediabot.models.responses.DiskErrorResponse;
 import ru.nsu.ccfit.nsumediabot.service.DiskService;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class DiskServiceImpl implements DiskService {
-    private final String DISK_RESOURCE_URL = "https://cloud-api.yandex.net/v1/disk/resources/upload";
-    private final String FILE_INFO_URL = "https://cloud-api.yandex.net/v1/disk/resources";
-    private final String MAKE_PUBLISH_URL = "https://cloud-api.yandex.net/v1/disk/resources/publish";
-    private final String DELETE_RESOURCE_URL = "https://cloud-api.yandex.net/v1/disk/resources";
+public class DiskServiceImpl
+        implements DiskService {
+
+    private static final String DIRECTORY_ALREADY_EXISTS_RESPONSE = "DiskPathPointsToExistentDirectoryError";
+    private static final String CREATE_DIRECTORY_URL = "https://cloud-api.yandex.net/v1/disk/resources";
+    private static final String GET_UPLOAD_URL_URL = "https://cloud-api.yandex.net/v1/disk/resources/upload";
+    private static final String PUBLISH_FILE_URL = "https://cloud-api.yandex.net/v1/disk/resources/publish";
+    private static final String GET_FILE_INFO_URL = "https://cloud-api.yandex.net/v1/disk/resources";
+    private static final String DELETE_FILE_URL = "https://cloud-api.yandex.net/v1/disk/resources";
 
     private final OkHttpClient client = new OkHttpClient();
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Value("${yandex.disk.api.token}")
     private String apiToken;
 
     @Override
-    public DiskFileDTO upload(File file) {
-        URL urlForUpload;
-        try {
-            urlForUpload = new URL(Objects.requireNonNull(getURL(file)));
-        } catch (MalformedURLException exception) {
-            log.error("Disk error", exception.getLocalizedMessage());
-            throw new DiskException(exception.getLocalizedMessage());
-        }
-
-        Request uploadRequest = new Request.Builder()
-                .url(urlForUpload)
+    public String uploadFile(String path, File file) {
+        createPath(path);
+        String uploadUrl = getUploadUrl(path);
+        Request request = new Request.Builder()
+                .url(uploadUrl)
                 .put(RequestBody.create(null, file))
                 .build();
 
         try {
-            Response uploadResponse = client.newCall(uploadRequest).execute();
-            if (!uploadResponse.isSuccessful()) {
-                handleError(uploadResponse);
-            }
-
-            return getFileInfo(file);
-        } catch (IOException exception) {
-            log.error(exception.getLocalizedMessage());
-            throw new DiskException(exception.getLocalizedMessage());
-        }
-    }
-
-    private String getURL(File file) {
-        Request request = new Request.Builder()
-                .url(DISK_RESOURCE_URL + "?path=" + file.getName())
-                .addHeader("Authorization", apiToken)
-                .get()
-                .build();
-
-        GetUploadUrlResponse getUploadUrlResponse = new GetUploadUrlResponse();
-        try {
             Response response = client.newCall(request).execute();
-
-            if (response.isSuccessful()) {
-                getUploadUrlResponse = objectMapper.readValue(response.body().string(), GetUploadUrlResponse.class);
-            } else {
-                handleError(response);
+            if (!response.isSuccessful()) {
+                throw new DiskException(mapToErrorResponse(response).getMessage());
             }
-        } catch (IOException exception) {
-            log.error(exception.getLocalizedMessage());
+        } catch (IOException e) {
+            throw new DiskException(e.getLocalizedMessage());
         }
 
-        return getUploadUrlResponse.getHref();
-    }
-
-    private DiskFileDTO getFileInfo(File file) {
-        makeFilePublish(file);
-
-        Request infoRequest = new Request.Builder()
-                .url(FILE_INFO_URL + "?path=" + file.getName())
-                .addHeader("Authorization", apiToken)
-                .get()
-                .build();
-
-        FileInfoResponse fileInfoResponse = new FileInfoResponse();
-        try {
-            Response infoResponse = client.newCall(infoRequest).execute();
-            if (infoResponse.isSuccessful()) {
-                fileInfoResponse = objectMapper.readValue(infoResponse.body().string(), FileInfoResponse.class);
-            } else {
-                handleError(infoResponse);
-            }
-        } catch (IOException exception) {
-            log.error("Disk error", exception.getLocalizedMessage());
-        }
-
-        DiskFileDTO dto = new DiskFileDTO();
-        dto.setFileLink(fileInfoResponse.getFile());
-        dto.setDiskFilePath(fileInfoResponse.getPath());
-
-        return dto;
-    }
-
-    private void makeFilePublish(File file) {
-
-        Request makePublicRequest = new Request.Builder()
-                .url(MAKE_PUBLISH_URL + "?path=" + "disk%3A%2F" + file.getName())
-                .addHeader("Authorization", apiToken)
-                .put(RequestBody.create(null, new byte[0]))
-                .build();
-
-        Response makePublicResponse;
-        try {
-            makePublicResponse = client.newCall(makePublicRequest).execute();
-
-            if (!makePublicResponse.isSuccessful()) {
-                handleError(makePublicResponse);
-            }
-        } catch (IOException exception) {
-            log.error(exception.getLocalizedMessage());
-            throw new DiskException(exception.getLocalizedMessage());
-        }
-    }
-
-    private void handleError(Response response) {
-        YandexDiskErrorResponse yandexDiskErrorResponse;
-        try {
-            yandexDiskErrorResponse = objectMapper.readValue(response.body().string(), YandexDiskErrorResponse.class);
-        } catch (IOException exception) {
-            throw new DiskException("Failed to parse error response");
-        }
-
-        throw new DiskException(yandexDiskErrorResponse.getMessage());
+        publishFile(path);
+        return getFileInfo(path);
     }
 
     @Override
-    public void delete(String diskFilePath) {
+    public void deleteFile(String path) {
+        URL url = HttpUrl.parse(DELETE_FILE_URL)
+                .newBuilder()
+                .addQueryParameter("path", path)
+                .build()
+                .url();
         Request request = new Request.Builder()
-                .url(DELETE_RESOURCE_URL + "?path=" + diskFilePath + "&permanently=true")
-                .addHeader("Authorization", apiToken)
+                .url(url)
+                .addHeader("Authorization", "OAuth " + apiToken)
                 .delete()
                 .build();
 
         try {
-            client.newCall(request).execute();
-        } catch (IOException exception) {
-            log.error(exception.getLocalizedMessage());
-            throw new DiskException(exception.getLocalizedMessage());
+            Response response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new DiskException(mapToErrorResponse(response).getMessage());
+            }
+        } catch (IOException e) {
+            throw new DiskException(e.getLocalizedMessage());
+        }
+    }
+
+    private DiskErrorResponse mapToErrorResponse(Response response) {
+        try {
+            return objectMapper.readValue(
+                    response.body().string(),
+                    DiskErrorResponse.class
+            );
+        } catch (IOException e) {
+            throw new DiskException(e.getLocalizedMessage());
+        }
+    }
+
+    private <T> T mapToSuccessResponse(Response response, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(response.body().string(), clazz);
+        } catch (IOException e) {
+            throw new DiskException(e.getLocalizedMessage());
+        }
+    }
+
+    private void createPath(String path) {
+        String[] dirs = path.split("/");
+        if (dirs[0].isEmpty()) {
+            dirs = Arrays.copyOfRange(dirs, 1, dirs.length - 1);
+        } else {
+            dirs = Arrays.copyOfRange(dirs, 0, dirs.length - 1);
+        }
+
+        StringBuilder createdPath = new StringBuilder();
+        for (String dir : dirs) {
+            createdPath.append("/").append(dir);
+            URL url = HttpUrl.parse(CREATE_DIRECTORY_URL)
+                    .newBuilder()
+                    .addQueryParameter("path", createdPath.toString())
+                    .build()
+                    .url();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .addHeader("Authorization", "OAuth " + apiToken)
+                    .put(RequestBody.create(null, ""))
+                    .build();
+
+            try {
+                Response response = client.newCall(request)
+                        .execute();
+                if (!response.isSuccessful()) {
+                    DiskErrorResponse diskErrorResponse = mapToErrorResponse(response);
+                    if (!Objects.equals(diskErrorResponse.getError(), DIRECTORY_ALREADY_EXISTS_RESPONSE)) {
+                        throw new DiskException(diskErrorResponse.getMessage());
+                    }
+                }
+            } catch (IOException e) {
+                throw new DiskException(e.getLocalizedMessage());
+            }
+        }
+    }
+
+    private String getUploadUrl(String path) {
+        URL url = HttpUrl.parse(GET_UPLOAD_URL_URL)
+                .newBuilder()
+                .addQueryParameter("path", path)
+                .build()
+                .url();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "OAuth " + apiToken)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request)
+                    .execute();
+            if (response.isSuccessful()) {
+                return mapToSuccessResponse(response, GetUploadUrlResponse.class).getHref();
+            }
+            throw new DiskException(mapToErrorResponse(response).getMessage());
+        } catch (IOException e) {
+            throw new DiskException(e.getLocalizedMessage());
+        }
+    }
+
+    private void publishFile(String path) {
+        URL url = HttpUrl.parse(PUBLISH_FILE_URL)
+                .newBuilder()
+                .addQueryParameter("path", path)
+                .build()
+                .url();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "OAuth " + apiToken)
+                .put(RequestBody.create(null, ""))
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (!response.isSuccessful()) {
+                throw new DiskException(mapToErrorResponse(response).getMessage());
+            }
+        } catch (IOException e) {
+            throw new DiskException(e.getLocalizedMessage());
+        }
+    }
+
+    private String getFileInfo(String path) {
+        URL url = HttpUrl.parse(GET_FILE_INFO_URL)
+                .newBuilder()
+                .addQueryParameter("path", path)
+                .build()
+                .url();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "OAuth " + apiToken)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                return mapToSuccessResponse(response, FileInfoResponse.class).getPublicUrl();
+            }
+            throw new DiskException(mapToErrorResponse(response).getMessage());
+        } catch (IOException e) {
+            throw new DiskException(e.getLocalizedMessage());
         }
     }
 }
