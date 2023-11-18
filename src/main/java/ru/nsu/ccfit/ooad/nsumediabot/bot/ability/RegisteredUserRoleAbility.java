1173ec459d3f25d123ba184725869016d6c720fd
ptrvsrg <s.petrov1@g.nsu.ru>
2023-11-18 22:39:57 +0700

    Добавил проверку размера загруженного файла - должен быть не более 20 МБ

--- src/main/java/ru/nsu/ccfit/ooad/nsumediabot/bot/ability/RegisteredUserRoleAbility.java
+++ src/main/java/ru/nsu/ccfit/ooad/nsumediabot/bot/ability/RegisteredUserRoleAbility.java
@@ -7,7 +7,6 @@ import org.telegram.abilitybots.api.util.AbilityExtension;
 import org.telegram.abilitybots.api.util.AbilityUtils;
 import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Document;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -51,34 +50,14 @@ public class RegisteredUserRoleAbility
     public Reply replyToOfferMaterial() {
         Reply offerMaterialResultReply = Reply.of(
                 (bot, upd) -> {
-                    // Create material
-                    String specializationName =
-                            DbUtils.getSpecializationFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
-                    Integer year = DbUtils.getYearFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
-                    Integer semester = DbUtils.getSemesterFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
-                    String subjectName = DbUtils.getSubjectFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
-                    String materialName = DbUtils.getMaterialNameFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
-                    MaterialDto materialDto = MaterialDto.builder()
-                            .subjectSpecializationName(specializationName)
-                            .subjectSemester((year - 1) * 2 + semester)
-                            .subjectName(subjectName)
-                            .name(materialName)
-                            .build();
-
-                    // Choose admin
-                    List<Long> adminChatIds = userService.loadAllAdmins()
-                            .stream()
-                            .map(UserDto::getChatId)
-                            .toList();
-                    if (adminChatIds.isEmpty()) {
-                        bot.silent().send(MessageUtils.ADMINS_NOT_FOUND, AbilityUtils.getChatId(upd));
+                    // Check file
+                    Document materialDocument = upd.getMessage().getDocument();
+                    if (materialDocument.getFileSize() > 20L * 1024L * 1024L) {
+                        bot.silent().send(MessageUtils.FILE_TOO_BIG, AbilityUtils.getChatId(upd));
                         return;
                     }
-                    Long adminChatId = adminChatIds.get(adminNumber % adminChatIds.size());
-                    adminNumber = (adminNumber + 1) % adminChatIds.size();
 
                     // Download material file
-                    Document materialDocument = upd.getMessage().getDocument();
                     String filePath = bot.silent().execute(
                             GetFile.builder()
                                     .fileId(materialDocument.getFileId())
@@ -92,6 +71,32 @@ public class RegisteredUserRoleAbility
                         materialFile.delete();
                         throw new RuntimeException(e);
                     }
+
+                    // Choose admin
+                    List<Long> adminChatIds = userService.loadAllAdmins()
+                            .stream()
+                            .map(UserDto::getChatId)
+                            .toList();
+                    if (adminChatIds.isEmpty()) {
+                        bot.silent().send(MessageUtils.ADMINS_NOT_FOUND, AbilityUtils.getChatId(upd));
+                        return;
+                    }
+                    Long adminChatId = adminChatIds.get(adminNumber % adminChatIds.size());
+                    adminNumber = (adminNumber + 1) % adminChatIds.size();
+
+                    // Create material
+                    String specializationName =
+                            DbUtils.getSpecializationFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
+                    Integer year = DbUtils.getYearFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
+                    Integer semester = DbUtils.getSemesterFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
+                    String subjectName = DbUtils.getSubjectFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
+                    String materialName = DbUtils.getMaterialNameFromUserMap(bot.db(), AbilityUtils.getChatId(upd));
+                    MaterialDto materialDto = MaterialDto.builder()
+                            .subjectSpecializationName(specializationName)
+                            .subjectSemester((year - 1) * 2 + semester)
+                            .subjectName(subjectName)
+                            .name(materialName)
+                            .build();
 
                     // Send to admin
                     Message message;
